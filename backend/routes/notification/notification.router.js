f8d3a33c10b0f2e99213aabf1afdb4f34c66e7f6
rajnish2025 <rajnish@veriqos.com>
2025-10-27 19:24:36 +0530

    minor changes to annoucement.

--- backend/routes/notification/notification.router.js
+++ backend/routes/notification/notification.router.js
@@ -4,8 +4,19 @@ const notificationController = require("../../Controller/Notification/notificati
 const fileController = require("../../Controller/file/fileController");
 const upload = require("../../multer/multer");
 
+// notice route
 router.post("/create-notice", notificationController.addNotice);
 router.post("/upload", upload.single("noticefile"), fileController.uploadFile);
 router.get("/all-notice", notificationController.getAllNotice);
+router.post('/delete-notice',notificationController.deleteNotice);
+router.patch('/update-notice',notificationController.updateNotice);
+
+//event route:
+router.post('/create-event',notificationController.createEvent);
+router.get('/all-event',notificationController.getAllEvents);
+router.patch('/update-event',notificationController.updateEvent);
+router.post('/delete-event',notificationController.deleteEvent);
+router.post("/event/upload", upload.single("eventfile"), fileController.uploadFile);
+
 
 module.exports = router;
